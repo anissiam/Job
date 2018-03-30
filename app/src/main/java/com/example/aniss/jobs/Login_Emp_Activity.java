4466911d0c9ddab2e8e18e5126b346760323c7dd
Anis Siam <anissiam@users.noreply.github.com>
2018-03-30 22:34:06 +0200

    //

---
+++ app/src/main/java/com/example/aniss/jobs/Login_Emp_Activity.java
@@ -0,0 +1,13 @@
+package com.example.aniss.jobs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Emp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__emp_);
+    }
+}
