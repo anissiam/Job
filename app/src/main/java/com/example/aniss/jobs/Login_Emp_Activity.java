2458339232679f663f389bf2b9f0bb1e5f29a147
Anis Siam <anissiam@users.noreply.github.com>
2018-03-31 10:49:14 +0300

    logon activity

--- app/src/main/java/com/example/aniss/jobs/Login_Emp_Activity.java
+++ app/src/main/java/com/example/aniss/jobs/Login_Emp_Activity.java
@@ -2,6 +2,7 @@ package com.example.aniss.jobs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_Emp_Activity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class Login_Emp_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__emp_);
 
+    }
+
+    public void signUp(View view) {
+    }
+
+    public void signIn(View view) {
+    }
+
+    public void signInWithGoogle(View view) {
     }
 }
